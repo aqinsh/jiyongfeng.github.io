5d29557d89cef6e47b6f34b262d3d9ec9f27afa9
aqsh <aqmails@gmail.com>
2018-03-19 16:04:06 +0800

    update

--- test.py
+++ test.py
@@ -1,24 +1,6 @@
-#import tkinter as Tk
-from tkinter import *
-from tkinter.scrolledtext import ScrolledText
-
-
-def load():
-    with open(filename.get()) as file:
-        contents.delete('1.0', END)
-        contents.insert(INSERT, file.read())
-
-
-top = Tk()
-top.title('Hello Word')
-
-contents = ScrolledText()
-contents.pack(side=BOTTOM, expand=True, fill=BOTH)
-
-filename = Entry()
-filename.pack(side=LEFT, expand=True, fill=X)
-
-Button(text='Open', command=load).pack(side=LEFT)
-
-
-mainloop()
+l = input()
+for i in range(input()):
+    if i == 'E':
+        break
+    l.join(i)
+print(l)
---
+++ vijos_P1911 珠心算测验.py
@@ -0,0 +1,11 @@
+n = int(input())
+l = input().split()
+l.sort()
+tip = []
+sum = 0
+new = [(x, y, z) for x in l for y in l for z in l if int(x) + int(y) == int(z) and int(x) < int(y)]
+tip.append(new[0][2])
+for i in new:
+    if tip.count(i[2]) == 0:
+        tip.append(i[2])
+print(len(tip))
