fb9bdbdf6c931ba2b3e164dcd5e1c7fdaa8364bb
aqsh <aqmails@gmail.com>
2018-04-16 14:47:58 +0800

    4.13update

--- Untitled.py
+++ Untitled.py
@@ -1,4 +1,15 @@
+import math
+def convert_recipe(recipe):
+    s = list(recipe.split(' '))
+    y = s
+    for i in range(len(s)):
+        if s[i] == 'tbsp':
+            y.insert(i+1, '('+str(math.ceil(eval(s[i-1])*15)) + 'g'+')')            
+        if s[i] == 'tsp':
+            y.insert(i+1, '('+str(math.ceil(eval(s[i-1])*5)) + 'g'+')')            
+    return ' '.join(y)
 
-width = bin(17)
-print(width)
-print(len(width))
+
+r = "Add to the mixing bowl and coat well with 1 tbsp of olive oil & 1/2 tbsp of dried dill"
+
+print(convert_recipe(r))
--- test.py
+++ test.py
@@ -1,6 +1,25 @@
-l = input()
-for i in range(input()):
-    if i == 'E':
-        break
-    l.join(i)
-print(l)
+import math
+def is_prime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for current in range(3, int(math.sqrt(number) + 1), 2):
+            if number % current == 0:
+                return False
+        return True
+    return False
+
+x = int(input())
+print(x)
+y  = []
+for i in range(2,int(x / 2)):
+    if x % i == 0:
+        y.append(i)
+print(y)
+
+for s in y:
+    test = is_prime(s)
+    if test:
+        print(s)
