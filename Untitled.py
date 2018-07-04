93aec1c56029d1f9b3fdd7216a7915bf89375c39
Steven <jiyongfeng@jiyongfengdeMacBook-Pro.local>
2018-07-04 17:30:08 +0800

    test

--- Untitled.py
+++ Untitled.py
@@ -1,15 +1,6 @@
-import math
-def convert_recipe(recipe):
-    s = list(recipe.split(' '))
-    y = s
-    for i in range(len(s)):
-        if s[i] == 'tbsp':
-            y.insert(i+1, '('+str(math.ceil(eval(s[i-1])*15)) + 'g'+')')            
-        if s[i] == 'tsp':
-            y.insert(i+1, '('+str(math.ceil(eval(s[i-1])*5)) + 'g'+')')            
-    return ' '.join(y)
-
-
-r = "Add to the mixing bowl and coat well with 1 tbsp of olive oil & 1/2 tbsp of dried dill"
-
-print(convert_recipe(r))
+string = 'hello world'
+l = string.split()
+new_list = []
+for i in l:
+    new_list.append(i.swapcase())
+    
