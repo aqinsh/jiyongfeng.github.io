28ef9125167d9aba50e72c3c601b42a97f4af589
Ji Yongfeng <aqmails@gmail.com>
2019-03-18 23:16:19 +0800

    hello world

--- hackerrank_collections-counter.py
+++ hackerrank_collections-counter.py
@@ -4,3 +4,4 @@ x = int(input("Please input the number of shoes:"))
 number = list(int(i) for i in (input(
     "Please input the space separated list of all the shoe sizes in hte shop:").split(' ')))
 N = int(input("Please input the number of customers:"))
+print(x)
