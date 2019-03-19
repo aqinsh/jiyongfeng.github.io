5715d0d89e7a620cd0bfb5b3d2ed7b0a35dcaf8a
aqinsh <aqmails@gmail.com>
2019-03-19 22:20:00 +0800

    finished

--- hackerrank_collections-counter.py
+++ hackerrank_collections-counter.py
@@ -1,7 +1,16 @@
 from collections import Counter
 
-x = int(input("Please input the number of shoes:"))
-number = list(int(i) for i in (input(
-    "Please input the space separated list of all the shoe sizes in hte shop:").split(' ')))
-N = int(input("Please input the number of customers:"))
-print(x)
+number_shoes = int(input("Please input the number of shoes:"))
+number_size = Counter(map(int, input().split()))
+number_customer = int(input("Please input the number of customers:"))
+
+income = 0
+
+for i in range(number_customer):
+    size, price = map(int, input().split())
+    if number_size[size]:
+        print(number_size)
+        print(number_size[size])
+        income += price
+        number_size[size] -= 1
+print(income)
