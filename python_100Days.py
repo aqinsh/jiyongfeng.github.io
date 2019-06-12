14db6e7f815dfb483181757d88ff26c29c4e560c
aqinsh <aqmails@gmail.com>
2019-06-12 16:35:37 +0800

    增加Python100Days代码

---
+++ python_100Days.py
@@ -0,0 +1,34 @@
+﻿import math
+
+
+def area_round(radius):
+    """
+    计算圆的面积
+    Version: 0.1
+    Author:Steven
+    Date: 2019-06-12
+     """
+    return math.pi * (radius ** 2)
+
+
+def perimeter_round(radius):
+    """
+    计算圆的周长
+    Version: 0.1
+    Author:Steven
+    Date: 2019-06-12
+    """
+    return 2 * math.pi * radius
+
+
+def checkleapyear(year):
+    """
+    判断闰年，如是，返回True，否则返回False
+    Version: 0.1
+    Author:Steven
+    Date: 2019-06-12
+    """
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
+    else:
+        return False
